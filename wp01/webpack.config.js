f17fdf60cd3f4524bf3ef4892e5477586206b48b
limuxiao <li19890814@163.com>
2018-07-12 09:51:08 +0800

    create project

---
+++ wp01/webpack.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack'); //访问内置的插件
+
+module.exports = {
+    entry: "./runoob1.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+	module: {
+        rules: [
+            { test: /\.css$/, loader: ('style-loader!css-loader') },
+        ]
+    },
+	plugins:[
+		new webpack.BannerPlugin('菜鸟教程 webpack 实例')
+	]
+};
